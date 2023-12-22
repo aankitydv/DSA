63872521cfcee0e3e2393646f03c28563877b466
aankitydv <aankityadav557@gmail.com>
2023-12-22 13:53:15 +0530

    done

--- program1.c
+++ program1.c
@@ -1,10 +1,4 @@
 #include <stdio.h>
 int main(){
-<<<<<<< HEAD
-    printf("hello worl");
-=======
-    printf(" world");
->>>>>>> 2041c4e26e2795e50bd42a537a51bb050d819502
-    return 0;
-//
+    printf("heloo")
 }
